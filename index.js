1a64da30712d921f1af45f159b0b26857fa89a54
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-29 23:54:08 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("running!")
+//sanity check valid
+
+const express = require('express')
+const db = require('./data/db')
+const postRouter = require('./data/post-router.js')
+//imports^
+
+const server = express()
+const port  = 3000
+server.listen(port, () => console.log('posts running on 3000'))
+server.use(express.json())
+server.use('/api/posts', postRouter)
+//server definition ^
+
+server.get('/', (req, res) => {
+    res.send('Post Data Retrieval')
+})
